1e85c4705f6e3c8122dd5058b8e00f4385ed45bb
Uri Levitsky <github@urilevitsky.com>
2025-04-11 09:56:02 +1000

    improved build script: added --push option

--- script/build-chart.sh
+++ script/build-chart.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 package=$(helm package chart | sed 's|^Successfully packaged chart and saved it to: ||')
-helm push $package oci://registry-1.docker.io/ulevitsky
+
+if [[ "$@" == *"--push"* ]]; then
+  helm push $package oci://registry-1.docker.io/ulevitsky
+fi
